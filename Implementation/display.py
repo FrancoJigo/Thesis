fe205c597d799c19474b038ae70046905df5fe44
Franco Jigu Pacana <frankeejigzz@gmail.com>
2018-10-18 23:01:25 +0800

    nanana

--- Implementation/display.py
+++ Implementation/display.py
@@ -2,18 +2,18 @@
 import math
 import sys
 try:
-from OpenGL.GLUT import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
+    from OpenGL.GLUT import *
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
 except:
-print ’ERROR:PyOpenGLnotinstalledproperly.’
-sys.exit ()
+    print 'ERROR: PyOpenGL not installed properly'
+    sys.exit ()
 try:
-from Hexint import Hexint
-from Hexarray import Hexarray
+    from Hexint import Hexint
+    from Hexarray import Hexarray
 except:
-print ’ERROR:Hexlibsarenotinstalled ’
-sys.exit( )
+    print 'ERROR: Hex libs are not installed'
+    sys.exit( )
 # default rotation and scale
 xrot ,yrot ,zrot = 0.0 ,0.0 ,0.0
 scale = -7.5
@@ -21,79 +21,78 @@ max = -9.99 e99
 scf = 1.0
 # initialise the open GL context
 def initGL( hdata , domain ):
-glClearColor (0.0, 1.0, 1.0, 0.0)
-glShadeModel( GL_SMOOTH )
-glPolygonMode( GL_FRONT , GL_FILL )
-glPolygonMode( GL_BACK , GL_LINE )
-glEnable( GL_DEPTH_TEST )
-# be efficient --make display list
-global hexList
-hexList = glGenLists (1)
-glNewList( hexList , GL_COMPILE )
-compute2DDisplayList( hdata ,domain )
-glEndList ()
+    glClearColor (0.0, 1.0, 1.0, 0.0)
+    glShadeModel( GL_SMOOTH )
+    glPolygonMode( GL_FRONT , GL_FILL )
+    glPolygonMode( GL_BACK , GL_LINE )
+    glEnable( GL_DEPTH_TEST )
+    # be efficient --make display list
+    global hexList
+    hexList = glGenLists (1)
+    glNewList( hexList , GL_COMPILE )
+    compute2DDisplayList( hdata ,domain )
+    glEndList ()
 # what to do when window is resized
 def reshapeGL( w,h ):
-if w>h:
-w=h
-elif h>w:
-h=w
-glViewport( 0,0, w,h )
-glMatrixMode( GL_PROJECTION )
-glLoadIdentity( )
-glFrustum (-1.0, 1.0, -1.0, 1.0, 5.0, 10.0)
-glMatrixMode( GL_MODELVIEW )
+    if w>h:
+        w=h
+    elif h>w:
+        h=w
+    glViewport( 0,0, w,h )
+    glMatrixMode( GL_PROJECTION )
+    glLoadIdentity( )
+    glFrustum (-1.0, 1.0, -1.0, 1.0, 5.0, 10.0)
+    glMatrixMode( GL_MODELVIEW )
 # the display function
 # uses a display list to store current
 # HIP image
 def displayGL ():
-global scf
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
-)
-glLoadIdentity( )
-glTranslatef( 0.0,0.0, scale )
-glScalef( scf ,scf ,scf )
-glRotatef( xrot , 1.0, 0.0, 0.0 )
-glRotatef( yrot , 0.0, 1.0, 0.0 )
-glRotatef( zrot , 0.0, 0.0, 1.0 )
-glCallList(hexList)
-glFlush ()
+    global scf
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity( )
+    glTranslatef( 0.0,0.0, scale )
+    glScalef( scf ,scf ,scf )
+    glRotatef( xrot , 1.0, 0.0, 0.0 )
+    glRotatef( yrot , 0.0, 1.0, 0.0 )
+    glRotatef( zrot , 0.0, 0.0, 1.0 )
+    glCallList(hexList)
+    glFlush ()
 # keyboard controls
 # escape : exit
 # x/X,y/Y,z/Z : rotate about x,y,z axis
 # s/S : scale the image
 def keyboardGL(key , x, y):
-global xrot ,yrot ,zrot
-global scale
-# quit
-if key == chr (27):
-sys.exit (0)
-# change rotation
-if key==chr (88): # X
-xrot += 0.5
-if key==chr(120): # x
-xrot -= 0.5
-if key==chr (89): # Y
-yrot += 0.5
-if key==chr(121): # y
-yrot -= 0.5
-if key==chr (90): # Z
-zrot += 0.5
-if key==chr(122): # z
-    zrot -= 0.5
-# change scale
-if key==chr (83): # S
-scale -= 0.1
-if key==chr(115): # s
-scale += 0.1
-# reset all vals
-if key==chr (82) or key==chr (114):
-scale = -10
-xrot ,yrot ,zrot = 0.0 ,0.0 ,0.0
-xrot ,yrot ,zrot = xrot %360, yrot %360, zrot %360
-if scale > -5.0: scale = -5.0
-if scale < -10.0: scale = -10.0
-displayGL( )
+    global xrot ,yrot ,zrot
+    global scale
+    # quit
+    if key == chr (27):
+        sys.exit (0)
+    # change rotation
+    if key==chr (88): # X
+        xrot += 0.5
+    if key==chr(120): # x
+        xrot -= 0.5
+    if key==chr (89): # Y
+        yrot += 0.5
+    if key==chr(121): # y
+        yrot -= 0.5
+    if key==chr (90): # Z
+        zrot += 0.5
+    if key==chr(122): # z
+        zrot -= 0.5
+    # change scale
+    if key==chr (83): # S
+        scale -= 0.1
+    if key==chr(115): # s
+        scale += 0.1
+    # reset all vals
+    if key==chr (82) or key==chr (114):
+        scale = -10
+        xrot ,yrot ,zrot = 0.0 ,0.0 ,0.0
+    xrot ,yrot ,zrot = xrot %360, yrot %360, zrot %360
+    if scale > -5.0: scale = -5.0
+    if scale < -10.0: scale = -10.0
+    displayGL( )
 # find the basis for plotting
 def findBasis( domain ,order):
 sqrt3 = math.sqrt (3.0)
