a4202386aea7a9dd550cb311405afd94dc71b493
francojigopacana <francojigo.pacana@g.msuiit.edu.ph>
2019-05-25 21:40:09 +0800

    added new model

--- CNN.py
+++ CNN.py
@@ -29,37 +29,25 @@ reg = 0.01
 classifier = Sequential()
 
 #Step1 Convolution
-classifier.add(Convolution2D(32,3,3,input_shape = (64,64,3),activation='relu',kernel_regularizer=l2(reg)))
+classifier.add(Convolution2D(32,3,3,input_shape = (64,64,3),activation='relu'))
 classifier.add(BatchNormalization())
-classifier.add(Dropout(0.5))
 #Step2 Pooling
 classifier.add(MaxPooling2D(pool_size = (2,2)))
-
-#Add Dropout
-classifier.add(Dropout(0.5))
 #Step3 Flattening
 classifier.add(Flatten())
-
 #Step4 Full Connection
-classifier.add(Dense(output_dim = 128, activation = 'relu',kernel_regularizer=l2(reg)))
-classifier.add(BatchNormalization())
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dropout(0.35))
 classifier.add(Dense(output_dim = 9, activation ='softmax'))
-
 #Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
 #Part 2 Fitting  the CNN to the images
 train_datagen = ImageDataGenerator(rotation_range=10,
                                     width_shift_range=0.05,
                                     height_shift_range=0.05,rescale = 1./255, shear_range = 0.2, zoom_range = 0.2,
                                     fill_mode='nearest', horizontal_flip = True)
-
 test_datagen = ImageDataGenerator(rescale = 1./255)
-
 training_set = train_datagen.flow_from_directory('images/Training', target_size = (64,64), batch_size = 25, shuffle = True, class_mode = 'categorical')
-
 test_set = test_datagen.flow_from_directory('images/Test', target_size = (64,64), batch_size = 25, shuffle = True, class_mode='categorical')
-
 classifier.fit_generator(training_set,steps_per_epoch = 8000, epochs = 10, validation_data = test_set, validation_steps = 800)
-
-classifier.save('CNN-4.model')
+classifier.save('CNN-gray.model')
---
+++ Square-implementation/images/Species5/20pics/Uniform_Cropping.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+def crop_image(file_name,new_height,new_width):
+
+    im = Image.open(file_name+".jpg")
+    width, height = im.size   
+
+    left = (width - new_width - 500)/2
+    top = (height - new_height -500)/2
+    right = (width + new_width -800)/2
+    bottom = (height + new_height+200)/2
+
+    crop_im = im.crop((left, top, right, bottom)) #Cropping Image 
+
+    crop_im.save(file_name+".jpg")  #Saving Images 
+
+new_width = 1000     #Enter the crop image width
+new_height = 1000    #Enter the crop image height
+i="test5"
+file_name = [i+".0",i+".01",i+".02",i+".03",i+".04",i+".05",i+".06",i+".07",i+".08",i+".09",i+".10",i+".11",i+".12",i+".13",i+".14",i+".15",i+".16",i+".17",i+".18",i+".19"] #Enter File Names
+#,i+".20",i+".21",i+".22",i+".23",i+".24",i+".25",i+".26",i+".27",i+".28",i+".29"
+
+for i in file_name:
+    crop_image(i,new_height,new_width)
+
+
+# crop_image('images/Species2/30pics/2.0.jpg',)
