f7d521afb004e6d531818da4e91ac4a1419da785
unknown <63917@MIN2-MCPACANA.NGCP.LOCAL>
2019-05-30 21:37:48 +0800

    added VGG16

---
+++ Pre-trained_Models/VGG16.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.utils import to_categorical
+import os
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from keras.applications import VGG16
+from keras import models
+from keras import layers
+from keras import optimizers
+
+
+
+vgg_conv = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(224, 224, 3))
+# vgg_conv.summary()
+train_dir = 'C:/Users/63917/Documents/Jigo/Thesis/images/Training'
+validation_dir = 'C:/Users/63917/Documents/Jigo/Thesis/images/Validation'
+ 
+nTrain = 270
+nVal = 180
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+train_features = np.zeros(shape=(nTrain, 7, 7, 512))
+train_labels = np.zeros(shape=(nTrain,9))
+
+train_generator = datagen.flow_from_directory(
+    train_dir,
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True)
+
+i = 0
+for inputs_batch, labels_batch in train_generator:
+    features_batch = vgg_conv.predict(inputs_batch)
+    train_features[i * batch_size : (i + 1) * batch_size] = features_batch
+    train_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+    i += 1
+    if i * batch_size >= nTrain:
+        break
+        
+train_features = np.reshape(train_features, (nTrain, 7 * 7 * 512))
+validation_features = np.zeros(shape=(nVal, 7, 7, 512))
+validation_labels = np.zeros(shape=(nVal,9))
+
+validation_generator = datagen.flow_from_directory(
+    validation_dir,
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=False)
+
+i = 0
+for inputs_batch, labels_batch in validation_generator:
+    features_batch = vgg_conv.predict(inputs_batch)
+    validation_features[i * batch_size : (i + 1) * batch_size] = features_batch
+    validation_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+    i += 1
+    if i * batch_size >= nVal:
+        break
+
+validation_features = np.reshape(validation_features, (nVal, 7 * 7 * 512))
+
+model = models.Sequential()
+model.add(layers.Dense(512, activation='relu', input_dim=7 * 7 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(9, activation='softmax'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-4),
+              loss='categorical_crossentropy',
+              metrics=['acc'])
+
+history = model.fit(train_features,
+                    train_labels,
+                    epochs=20,
+                    batch_size=batch_size,
+                    validation_data=(validation_features,validation_labels))
+classifier.save('VGG16_1.model')
