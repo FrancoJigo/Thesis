ccddf2ed75522c2625e299ff85d719b90a472fb6
francojigopacana <francojigo.pacana@g.msuiit.edu.ph>
2019-05-30 21:31:15 +0800

    added nothin but deleted something

--- feature_selection.py
+++ feature_selection.py
@@ -4,6 +4,9 @@ from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 import warnings
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+from sklearn.ensemble import ExtraTreesClassifier
 warnings.filterwarnings("ignore", category=FutureWarning)
 # data = pd.read_csv("features.csv")
 # data.head()
@@ -20,18 +23,25 @@ for name in names:  # Iterate over chosen columns
 	dataframe[name] = pd.to_numeric(dataframe[name])
 
 array = dataframe.values
-X = array[:,1:11]
-Y = array[:,11]
-
+# X = array[:,1:11]
+# Y = array[:,11]
+X = dataframe.iloc[:,1:11]  #independent columns
+Y = dataframe.iloc[:,11]    #target column i.e price range
 # print(X)
 # print(array[0,0])
 #Feature extraction
-model = LogisticRegression()
-rfe = RFE(model, 6)
-fit = rfe.fit(X, Y)
-print("Num Features: %s" % (fit.n_features_))
-print("Selected Features: %s" % (fit.support_))
-print("Feature Ranking: %s" % (fit.ranking_))
+# model = LogisticRegression()
+model = ExtraTreesClassifier()
+# rfe = RFE(model, 6)
+# fit = rfe.fit(X, Y)
+model.fit(X,Y)
+print(model.feature_importances_)
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(10).plot(kind='barh')
+plt.show()
+# print("Num Features: %s" % (fit.n_features_))
+# print("Selected Features: %s" % (fit.support_))
+# print("Feature Ranking: %s" % (fit.ranking_))
 
 # Num Features: 6
 # Selected Features: [False False  True  True  True False  True False  True  True]
---
+++ show_matrix.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(20,10))
+columns = 5
+for i, image in enumerate(images):
+    plt.subplot(len(images) / columns + 1, columns, i + 1)
+    plt.imshow(image)
